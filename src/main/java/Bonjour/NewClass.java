edbe99c4e8a1fe6ebe417b077f75d00001fd145f
thierno-bah <54667738+thierno-bah@users.noreply.github.com>
2020-04-24 19:48:58 -0400

    RAJOUTER PRINTLN

--- src/main/java/Bonjour/NewClass.java
+++ src/main/java/Bonjour/NewClass.java
@@ -14,6 +14,9 @@ public class NewClass {
     public static void main(String[] args) {
         
         System.out.println("Bonjour Les amis");
+         System.out.println("Bonjour Les amis");
+                 System.out.println("Bonjour Les amis");
+
     }
     
 }
